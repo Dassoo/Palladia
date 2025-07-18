84c4ab4e7acff382f712e6d4d687648e5d515e0a
Federico Dassiè <federico.dassie@gmail.com>
2025-07-18 17:29:43 +0200

    update

--- evaluation/graph.py
+++ evaluation/graph.py
@@ -71,5 +71,5 @@ def create_graph(path: str):
     plt.savefig(f"{path}.png")
 
 
-if  __name__ == "__main__":
-    create_graph("results/GT4HistOCR/corpus/EarlyModernLatin/1471-Orthographia-Tortellius.json")
+# if  __name__ == "__main__":
+#     create_graph("results/GT4HistOCR/corpus/EarlyModernLatin/1471-Orthographia-Tortellius.json")
